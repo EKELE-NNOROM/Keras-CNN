b413acf4b33353996204c8088746e7252e6e4475
ekelennorom <ekele.nnorom@gmail.com>
2018-07-27 11:25:27 -0400

    CNN Keras

---
+++ my_cnn..py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 22:05:42 2018
+
+@author: ekele
+"""
+from PIL import Image
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPool2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+from keras.layers import Convolution2D
+
+classifier = Sequential()
+
+classifier.add(Convolution2D(32, 3, padding = 'same', input_shape = (128, 128, 3), activation = 'relu'))
+
+classifier.add(MaxPool2D(pool_size=(2, 2)))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                target_size=(128, 128),
+                                                batch_size=32,
+                                                class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(128, 128),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+classifier.fit_generator(training_set,
+                        steps_per_epoch=8000,
+                        epochs=50,
+                        validation_data=test_set,
+                        validation_steps=2000)
